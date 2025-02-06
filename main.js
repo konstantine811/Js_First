44903443ef7289490900c2ec6cb80c28d3f7dfc0
konstantine811 <galystyan777@gmail.com>
2025-02-06 19:27:22 +0200

    new changes

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-const h1El = document.querySelector("h1");
-const btn = document.getElementById("btn");
-btn.addEventListener("click", hideText);
-let isTextVisible = true;
-function hideText() {
-  h1El.style.visibility = isTextVisible ? "hidden" : "visible";
-  btn.textContent = isTextVisible ? "Hide of text" : "Show of text";
-  isTextVisible = !isTextVisible;
-}
+
